83904accc060e9046de055ec766c7a15216fc345
Sumeet Agarwal <sumeet.a@gmail.com>
2024-12-08 21:41:01 -0800

    wip

--- day06/soln.sup.js
+++ day06/soln.sup.js
@@ -25,16 +25,15 @@ function part1(input) {
   return visited.size;
 }
 
+
 function part2(input) {
-  const acc = new Set();
-  function search(grid, pos, dir, depth) {
-    let count = 0;
-    const visited = {};
+  function cacheKey(pos, dir) { return `${pos}|${dir}`; }
+  function search(grid, pos) {
+    let dir = [-1, 0];
+    const visited = new Set();
     while (true) {
-      (visited[pos] ||= []).push(String(dir));
       pos[0] += dir[0];
       pos[1] += dir[1];
-      if (depth > 0 && (visited[pos] || []).includes(String(dir))) return 1;
       if (oob(grid, pos)) { break; }
       if (grid[pos[0]][pos[1]] === "#") {
         pos[0] -= dir[0];
@@ -42,35 +41,32 @@ function part2(input) {
         dir = rot90(dir);
         pos[0] += dir[0];
         pos[1] += dir[1];
-      } else if (depth === 0) {
-        const hypoPos = pos.slice();
-        hypoPos[0] -= dir[0];
-        hypoPos[1] -= dir[1];
-        const hypoDir = rot90(dir);
-        hypoPos[0] += hypoDir[0];
-        hypoPos[1] += hypoDir[1];
-        const grid2 = JSON.parse(JSON.stringify(grid));
-        grid2[pos[0]][pos[1]] = '#'
-        let res;
-        if ((res = search(grid2, hypoPos, hypoDir, depth+1)) > 0) {
-          acc.add(String(pos));
-          count += res;
-        }
       }
+      if (visited.has(cacheKey(pos, dir))) return 'LOOP';
+      visited.add(cacheKey(pos, dir));
     }
-    return count;
+    return visited;
   }
   const initGrid = input.trim().split("\n").map(line => line.split(""));
-  let startingPos;
+  let initPos;
   for (let y = 0; y < initGrid.length; y++) {
     for (let x = 0; x < initGrid[y].length; x++) {
-      if (initGrid[y][x] === "^") startingPos = [y, x];
+      if (initGrid[y][x] === "^") initPos = [y, x];
     }
   }
-  const ret = search(initGrid, startingPos, [-1, 0], {}, 0);
-  //console.log(initGrid.map(l => l.join("")).join("\n"));
-  return acc.size;
-  //return ret;
+  let visited = search(clone(initGrid), clone(initPos));
+  visited = [...new Set([...visited].map(v => v.split("|")[0]))];
+  let count = 0;
+  for (const [y, x] of visited.map(v => v.split(',').map(Number))) {
+    const grid = clone(initGrid);
+    if (grid[y][x] === '^') continue;
+    if (grid[y][x] === '#') throw new Error("visited a #, shouldn't happen");
+    grid[y][x] = '#';
+    if (search(clone(grid), clone(initPos)) === 'LOOP') {
+      count++;
+    }
+  }
+  return count;
 }
 
 function oob(grid, pos) {
@@ -78,17 +74,15 @@ function oob(grid, pos) {
     pos[1] >= grid[0].length;
 }
 
-function rot90(dir) {
-  if (dir[0] === -1 && dir[1] === 0) return [0, 1]; // up -> right
-  if (dir[0] === 0 && dir[1] === 1) return [1, 0]; // right -> down
-  if (dir[0] === 1 && dir[1] === 0) return [0, -1]; // down -> left
-  return [-1, 0];// left -> up
-}
+function rot90(dir) { return [dir[1], -dir[0]]; }
+
+function clone(o) { return JSON.parse(JSON.stringify(o)); }
 
 const fs = require('fs');
-//const input = fs.readFileSync('input.txt', 'utf8');
 const input = fs.readFileSync('input.txt', 'utf8');
+//const input = fs.readFileSync('sample.txt', 'utf8');
 console.log('part 1');
 console.log(part1(input));
 console.log('part 2');
 console.log(part2(input));
+
