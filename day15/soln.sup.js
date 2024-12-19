3883a2b93967d6ac74260f6bf0b93d7d7b458632
Sumeet Agarwal <sumeet.a@gmail.com>
2024-12-18 22:08:34 -0800

    finish day15 part1

---
+++ day15/soln.sup.js
@@ -0,0 +1,89 @@
+function part1(input) {
+  let [grid, instructions] = input.trim().split('\n\n');
+  grid = grid.split('\n').map(l => l.split(''));
+  instructions = instructions.replaceAll('\n', '');
+  map = {};
+  let playerPos = [0, 0];
+  for (const [y, line] of grid.entries()) {
+    for (const [x, c] of line.entries()) {
+      if (c === '@') {
+        playerPos = [y, x];
+        map[[y, x]] = '.';
+      } else {
+        map[[y, x]] = c;
+      }
+    }
+  }
+
+  outer: for (const instr of instructions) {
+    let dir;
+    switch (instr) {
+      case '^':
+        dir = [-1, 0];
+        break;
+      case '>':
+        dir = [0, 1];
+        break;
+      case 'v':
+        dir = [1, 0];
+        break;
+      case '<':
+        dir = [0, -1];
+        break;
+      default:
+        throw new Error(`Invalid instruction: ${instr}`);
+    }
+
+    const stack = [];
+    let nextPos = playerPos;
+    inner: while (true) {
+      nextPos = [nextPos[0] + dir[0], nextPos[1] + dir[1]];
+      switch (map[nextPos]) {
+        case '#': continue outer;
+        case '.': 
+          stack.push(nextPos);
+          break inner;
+        case 'O':
+          stack.push(nextPos);
+          continue inner; 
+        default:
+          throw new Error(`Invalid grid item: ${map[nextPos]}`);
+      }
+    }
+    if (stack.length === 0) continue;
+    while (stack.length > 1) {
+      let next = stack.pop();
+      let prev = [next[0] - dir[0], next[1] - dir[1]];
+      map[prev] = '.';
+      map[next] = 'O';
+    }
+    playerPos = stack.pop();
+
+    //for (let y = 0; y < grid.length; y++) {
+    //  let line = '';
+    //  for (let x = 0; x < grid[y].length; x++) {
+    //    if (playerPos[0] === y && playerPos[1] === x) {
+    //      line += '@';
+    //    } else {
+    //      line += map[[y, x]];
+    //    }
+    //  }
+    //  console.log(line);
+    //}
+  }
+
+  let total = 0;
+  for (const [coord, c] of Object.entries(map)) {
+    if (c === 'O') {
+      const [y, x] = coord.split(',').map(Number);
+      total += 100 * y + x;
+    }
+  }
+  return total;
+}
+
+const fs = require('fs');
+const input = fs.readFileSync('input.txt', 'utf8');
+//const input = fs.readFileSync('sample-large.txt', 'utf8');
+
+console.log(part1(input));
