68405aca2c43dfd72518d68ebda017d4b9b24553
Sumeet Agarwal <sumeet.a@gmail.com>
2024-12-21 16:27:59 -0800

    wip

--- day21/day21/src/main.rs
+++ day21/day21/src/main.rs
@@ -1,18 +1,22 @@
+#![feature(slice_partition_dedup)]
+
 use std::collections::BTreeMap;
 use std::iter::{once, repeat_n};
 use std::sync::LazyLock;
 
 fn main() {
-    let mut pads = once(Pad::num())
-        .chain(repeat_n(Pad::dir(), 3))
-        .collect::<Vec<_>>();
+    dbg!(Pad::dir());
 
-    let code = SAMPLE.split("\n").next().unwrap();
-
-    dbg!(code);
+    // let mut pads = once(Pad::num())
+    //     .chain(repeat_n(Pad::dir(), 3))
+    //     .collect::<Vec<_>>();
+    //
+    // let code = SAMPLE.split("\n").next().unwrap();
+    //
+    // dbg!(code);
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Grid {
     char_by_pos: BTreeMap<(i8, i8), char>,
     pos_by_char: BTreeMap<char, (i8, i8)>,
@@ -27,9 +31,9 @@ impl Grid {
         }
     }
 
-    fn get_char(&self, c: char) -> Option<(i8, i8)> {
-        self.pos_by_char.get(&c).copied()
-    }
+    // fn get_char(&self, c: char) -> Option<(i8, i8)> {
+    //     self.pos_by_char.get(&c).copied()
+    // }
 
     // fn get_pos(&self, p: (i8, i8)) -> Option<char> {
     //     self.char_by_pos.get(&p).copied()
@@ -56,7 +60,7 @@ static DIR_BY_CHAR: LazyLock<BTreeMap<char, (i8, i8)>> = LazyLock::new(|| {
 static CHAR_BY_DIR: LazyLock<BTreeMap<(i8, i8), char>> =
     LazyLock::new(|| DIR_BY_CHAR.iter().map(|(c, d)| (*d, *c)).collect());
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Pad {
     grid: Grid,
     all_shortest_paths: BTreeMap<(char, char), Vec<Vec<char>>>,
@@ -94,27 +98,34 @@ impl Pad {
                 if (dy != 0) {
                     let mut path = vec![];
                     let mut cur = (*from_y, *from_x);
+                    let mut dy = dy;
+                    let mut dx = dx;
                     while (dy != 0 || dx != 0) {
-                        while (dy != 0) {
-                            let next = (cur.0 + dy.signum(), cur.1);
-                            if !grid.has_pos(next) {
-                                break;
-                            }
-                            path.push(if dy > 0 { 'v' } else { '^' });
-                            cur = next;
-                        }
-                        while (dx != 0) {
-                            let next = (cur.0, cur.1 + dx.signum());
-                            if !grid.has_pos(next) {
-                                break;
-                            }
-                            path.push(if dx > 0 { '>' } else { '<' });
-                            cur = next;
-                        }
+                        dy = Self::move_in_y_direction(&grid, dy, &mut path, &mut cur);
+                        dx = Self::move_in_x_direction(&grid, dx, &mut path, &mut cur);
+                    }
+                    if (path.len() > 0) {
+                        paths.push(path);
+                    }
+                }
+                if (dx != 0) {
+                    let mut path = vec![];
+                    let mut cur = (*from_y, *from_x);
+                    let mut dy = dy;
+                    let mut dx = dx;
+                    while (dy != 0 || dx != 0) {
+                        dx = Self::move_in_x_direction(&grid, dx, &mut path, &mut cur);
+                        dy = Self::move_in_y_direction(&grid, dy, &mut path, &mut cur);
+                    }
+                    if (path.len() > 0) {
+                        paths.push(path);
                     }
                 }
 
-                all_shortest_paths.insert((*from, *to), paths);
+                let min_path_length = paths.iter().map(|p| p.len()).min().unwrap();
+                paths.retain(|p| p.len() == min_path_length);
+                let (dedup, _) = paths.partition_dedup();
+                all_shortest_paths.insert((*from, *to), dedup.to_vec());
             }
         }
 
@@ -123,6 +134,42 @@ impl Pad {
             current: 'A',
             all_shortest_paths,
         }
+    }
+
+    fn move_in_x_direction(
+        grid: &Grid,
+        mut dx: i8,
+        path: &mut Vec<char>,
+        cur: &mut (i8, i8),
+    ) -> i8 {
+        while (dx != 0) {
+            let next = (cur.0, cur.1 + dx.signum());
+            if !grid.has_pos(next) {
+                break;
+            }
+            path.push(if dx > 0 { '>' } else { '<' });
+            *cur = next;
+            dx -= dx.signum();
+        }
+        dx
+    }
+
+    fn move_in_y_direction(
+        grid: &Grid,
+        mut dy: i8,
+        path: &mut Vec<char>,
+        cur: &mut (i8, i8),
+    ) -> i8 {
+        while (dy != 0) {
+            let next = (cur.0 + dy.signum(), cur.1);
+            if !grid.has_pos(next) {
+                break;
+            }
+            path.push(if dy > 0 { 'v' } else { '^' });
+            *cur = next;
+            dy -= dy.signum();
+        }
+        dy
     }
 
     //     +---+---+
