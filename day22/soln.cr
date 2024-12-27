c1cb29e06bcd037b932653634cc03fbdc85102d8
Sumeet Agarwal <sumeet.a@gmail.com>
2024-12-26 20:08:20 -0800

    wip

--- day22/soln.cr
+++ day22/soln.cr
@@ -14,8 +14,39 @@ def evolve(secret)
   prune mix(secret * 2048, secret)
 end
 
-res = File.read("input.txt").lines.sum do |line|
-  2000.times.reduce(BigInt.new line) { |acc, _| evolve(acc) }
+nums = File.read("input.txt").lines.map { |line| BigInt.new line }
+
+res = nums.sum do |num|
+  2000.times.reduce(num) { |acc, _| evolve(acc) }
 end
 
 puts "part 1: #{res}"
+
+alias Seq = Tuple(Int8, Int8, Int8, Int8)
+
+hashes = nums.map do |num|
+  first_by_seq = {} of Seq => Int8
+  last_4 = [] of Int8
+  2000.times do
+    new_num = evolve(num)
+    new_price = (new_num % 10).to_i8
+    old_price = (num % 10).to_i8
+    price_diff = (new_price - old_price).to_i8
+    last_4 = last_4.push(price_diff).last(4)
+    first_by_seq[Seq.from(last_4)] ||= new_price if last_4.size == 4
+    num = new_num
+  end
+  first_by_seq
+end
+
+union_of_keys = hashes.reduce(Set(Seq).new) do |acc, h|
+  acc |= h.keys.to_set
+end
+
+res = union_of_keys.max_of do |seq|
+  hashes.sum { |h| h.fetch(seq, 0) }
+end
+
+# 1747 doesn't work
+
+puts "part 2: #{res}"
