7494531c4287e637223df16f6bdb02e05f9e3ff7
Sumeet Agarwal <sumeet.a@gmail.com>
2024-12-26 16:59:18 -0800

    finish day18

--- day18/soln.cr
+++ day18/soln.cr
@@ -1,17 +1,24 @@
-SIZE     = 7
-FILENAME = "sample.txt"
-TAKE     = 12
+# SIZE     = 7
+# FILENAME = "sample.txt"
+# TAKE     = 12
+
+SIZE     = 71
+FILENAME = "input.txt"
+TAKE     = 1024
+
 alias Point = Tuple(Int32, Int32)
 
-GRID = SIZE.times.flat_map do |y|
-  SIZE.times.map do |x|
-    { {y, x}, '.' }
+GRID = SIZE.times.flat_map do |x|
+  SIZE.times.map do |y|
+    { {x, y}, '.' }
   end
 end.to_h
 
-File.read(FILENAME).lines.first(TAKE).each do |line|
-  GRID[Point.from(line.split(",").map(&.to_i))] = '#'
+POINTS = File.read(FILENAME).lines.map do |line|
+  Point.from(line.split(",").map(&.to_i))
 end
+
+POINTS.first(TAKE).each { |point| GRID[point] = '#' }
 
 START = {0, 0}
 GOAL  = {SIZE - 1, SIZE - 1}
@@ -30,23 +37,22 @@ class State
   def next_states
     [{0, 1}, {1, 0}, {0, -1}, {-1, 0}].map do |dydx|
       new_pos = Point.from(@pos.zip(dydx).map(&.sum))
-      next if GRID[new_pos]? == '#'
+      next unless GRID[new_pos]? == '.'
       State.new(new_pos, @steps + 1)
     end.compact
   end
 end
 
-def go
-  visited = {START => 0}
+def part1
   q = [State.new START]
+  visited = {START => q[0].heuristic}
   until q.empty?
     current = q.shift
-
     return current if current.pos == GOAL
 
     current.next_states.each do |next_state|
-      next if visited.fetch(next_state.pos, Int32::MAX) <= next_state.steps
-      visited[next_state.pos] = next_state.steps
+      next if visited.fetch(next_state.pos, Int32::MAX) <= next_state.heuristic
+      visited[next_state.pos] = next_state.heuristic
       q << next_state
     end
 
@@ -54,4 +60,27 @@ def go
   end
 end
 
-p go
+puts "part 1: #{part1.not_nil!.steps}"
+
+def is_end_reachable(pos, cache, seen)
+  if (cached = cache[pos]?) != nil
+    return cached
+  end
+
+  seen.add(pos)
+
+  nexts = [{0, 1}, {1, 0}, {0, -1}, {-1, 0}].map do |dydx|
+    new_pos = Point.from(pos.zip(dydx).map(&.sum))
+    new_pos if GRID[new_pos]? == '.' && !seen.includes?(new_pos)
+  end.compact
+  return true if nexts.includes?(GOAL)
+  nexts.any? { |n| is_end_reachable(n, cache, seen) }
+end
+
+POINTS.skip(TAKE).each do |point|
+  GRID[point] = '#'
+  seen = Set(Point).new
+  if !is_end_reachable(START, {} of Point => Bool, seen)
+    break puts "part 2: #{point.join(",")}"
+  end
+end
